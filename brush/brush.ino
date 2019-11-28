aeccc9ad58ce72cd2fef11547372f679157bf72f
Geo <41096155+Georqe@users.noreply.github.com>
2019-11-28 10:34:45 -0500

    updated brush code

--- brush/brush.ino
+++ brush/brush.ino
@@ -11,39 +11,46 @@ void setup() {
   s0.attach(A0);
   s1.attach(A1);
   s2.attach(A2); 
+
 }
-
-
+void loop(){
  //Position 1
-  s0.write(angle);
-  s1.write(angle);
-  s1.write(angle);
-  delay(1000);
+  s0.write(100);
+  s1.write(170);
+  s2.write(100);
+  delay(5000);
+
 
 
   
  //Position 2
-  s0.write(angle);
-  s1.write(angle);
-  s1.write(angle);
-  delay(1000);
+  s0.write(75);
+  s1.write(180);
+  s2.write(10);
+  delay(5000);
 
 
    //Position 3
-  s0.write(angle);
-  s1.write(angle);
-  s1.write(angle);
-  delay(1000);
+  s0.write(125);
+  s1.write(180);
+  s2.write(10);
+  delay(5000);
 
 
    //Position 4
-  s0.write(angle);
-  s1.write(angle);
-  s1.write(angle);
-  delay(1000);
+  s0.write(125);
+  s1.write(0);
+  s2.write(0);
+  delay(5000);
 
 
-  
+    //postion 5
+  s0.write(75);
+  s1.write(0);
+  s2.write(0);
+  delay(5000);
+
+}
 
 
 
